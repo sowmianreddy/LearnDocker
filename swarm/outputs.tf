08b2169947149b23504178c71f1aa951500a81af
Prasad <prasad.telasula@yahoo.com>
2019-10-29 02:16:15 +0530

    Provision Swarm Master Node

--- swarm/outputs.tf
+++ swarm/outputs.tf
@@ -6,19 +6,19 @@ output "master_priv_ip" {
   value = aws_instance.swarm-master.*.private_ip
 }
 
-output "worker1_pub_ip" {
-  value = aws_instance.worker-1.*.public_ip
-}
+# output "worker1_pub_ip" {
+#   value = aws_instance.worker-1.*.public_ip
+# }
 
-output "worker1_priv_ip" {
-  value = aws_instance.worker-1.*.private_ip
-}
+# output "worker1_priv_ip" {
+#   value = aws_instance.worker-1.*.private_ip
+# }
 
-output "worker2_pub_ip" {
-  value = aws_instance.worker-2.*.public_ip
-}
+# output "worker2_pub_ip" {
+#   value = aws_instance.worker-2.*.public_ip
+# }
 
-output "worker2_priv_ip" {
-  value = aws_instance.worker-2.*.private_ip
-}
+# output "worker2_priv_ip" {
+#   value = aws_instance.worker-2.*.private_ip
+# }
 
