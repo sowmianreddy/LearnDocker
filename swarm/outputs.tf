73c05900c1f6006013ddb2a7b710bcf35b9ac28d
Prasad <prasad.telasula@yahoo.com>
2019-10-29 08:24:44 +0530

    Added worker node 2

--- swarm/outputs.tf
+++ swarm/outputs.tf
@@ -14,11 +14,11 @@ output "worker1_priv_ip" {
   value = aws_instance.worker-1.*.private_ip
 }
 
-# output "worker2_pub_ip" {
-#   value = aws_instance.worker-2.*.public_ip
-# }
+output "worker2_pub_ip" {
+  value = aws_instance.worker-2.*.public_ip
+}
 
-# output "worker2_priv_ip" {
-#   value = aws_instance.worker-2.*.private_ip
-# }
+output "worker2_priv_ip" {
+  value = aws_instance.worker-2.*.private_ip
+}
 
